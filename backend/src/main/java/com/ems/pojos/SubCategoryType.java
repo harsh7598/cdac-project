7f8c5e7114c6f7c0e51e14cf4b74f96a41936941
NimishPatil4 <nimishpatil4@gmail.com>
2022-02-18 09:37:01 +0530

    remove old backend

--- backend/src/main/java/com/ems/pojos/SubCategoryType.java
+++
@@ -1,5 +0,0 @@
-package com.ems.pojos;
-
-public enum SubCategoryType {
-RICE,ROTI
-}
--- backend/src/main/java/com/ems/pojos/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.ems.pojos;
-
-public enum UserRole {
-	ROLE_CUSTOMER, ROLE_ADMIN,ROLE_MANAGER,ROLE_EMPLOYEE
-}
