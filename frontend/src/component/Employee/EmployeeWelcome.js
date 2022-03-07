fc26c8a499a1f0afb2c0ebf4389b42c8e75ebeb2
nimishpatil4 <nimishpatil4@gmail.com>
2022-03-07 10:25:13 +0530

    ....

--- frontend/src/component/Employee/EmployeeWelcome.js
+++ frontend/src/component/Employee/EmployeeWelcome.js
@@ -4,30 +4,30 @@ import { url } from "../common/constants";
 import { Link } from "react-router-dom";
 const EmployeeWelcome = () => {
 const [status, setstatus] = useState("")
-  const [tasks, setTasks] = useState([]);
-  const init=()=>{
-    const token=JSON.parse(localStorage.getItem("jwttoken"));
-      axios.get(url+"/asigntasks",{headers:{"authorization":`Bearer ${token}`}}).then(Response => {
-        console.log('Printing Menu data', Response.data);
-        setTasks(Response.data);
-    })
-        .catch(error => {
-            console.log('Something went wrong', error);
-        })
+const [tasks, setTasks] = useState([]);
+const init=()=>{
+  const token=JSON.parse(localStorage.getItem("jwttoken"));
+  axios.get(url+"/asigntasks",{headers:{"authorization":`Bearer ${token}`}}).then(Response => {
+    console.log('Printing Menu data', Response.data);
+    setTasks(Response.data);
+  })
+  .catch(error => {
+    console.log('Something went wrong', error);
+  })
   }
-
+  
   const Handledone=(t)=>{
     axios.put(url+"/updatetask",t).then(Response => {
       console.log('Printing Menu data', Response.data);
-  })
-      .catch(error => {
-          console.log('Something went wrong', error);
-      });
+    })
+    .catch(error => {
+      console.log('Something went wrong', error);
+    });
   }
-
+  
   useEffect(() => {
     init();
-  }, [])
+    }, [])
   return (
     // <div className=" position-absolute top-50 start-50 translate-middle h-100 w-100">
     <div className="forms-container">
@@ -38,13 +38,12 @@ const [status, setstatus] = useState("")
             <div className="py-5 px-4 h-100" key={task.id}>
                 <div className="services__box py-5 px-2 h-100 border border-2 border-white">
                   <div><p className="py-5 fs-2">{task.todo}</p></div>
-                  <div><p className="py-5 fs-2">{task.status}</p></div>
                   <select name="status" id="status" className="input-fields-mod" defaultValue={task.status} onChange={(e) => { task.status=e.target.value;Handledone(task) }}>
                     <option value="completed">Completed</option>
                     <option value="in progress">In Progress</option>
                     <option value="Not Yet Started">Not Yet Started</option>
                   </select>
-                  <button className="btn btn primary" onClick={Handledone(task)}>done</button>
+                  {/* <div><p className="py-5 fs-2">{task.status}</p></div> */}
                 </div>
             </div>
 
