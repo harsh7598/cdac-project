18c3ed69ba6c958b51ac11d5090d5a24adef17a7
RohitWarke <rohitwarke024@gmail.com>
2022-03-11 20:11:31 +0530

    added delete employee confirmation popup

--- frontend/src/component/Employee/ViewEmployee.js
+++ frontend/src/component/Employee/ViewEmployee.js
@@ -6,9 +6,13 @@ import { Link } from 'react-router-dom';
 
 const ViewEmployee = () => {
     const [Employee, setEmployee] = useState([]);
+    const [Id, setId] = useState();
 
-
+    useEffect(() => {
+        init();
+    }, []);
     const HandleRemove = (id) => {
+        console.log("id is" + id);
         axios.delete(url + "/deleteemployee/" + id).then(Response => {
             console.log('delete Employee successfully');
             init();
@@ -17,6 +21,8 @@ const ViewEmployee = () => {
                 console.log('Something went wrong', error);
             })
     }
+
+
 
     const init = () => {
         axios.get(url + "/allemployees")
@@ -28,29 +34,65 @@ const ViewEmployee = () => {
                 console.log('Something went wrong', error);
             })
     };
-    useEffect(() => {
-        init();
-    }, []);
+
     return (
 
         <div className="forms-container">
             <div className="py-5 text-white my-5">
-                <div colSpan="2" className="fw-bold pt-5 display-6">
+                <div  className="fw-bold pt-5 display-6">
                     Employees
                 </div>
-                <Link className='btn btn-l' to='/regemployee' >register Employee</Link>
-                <div className="py-4 list-items">
-                    {Employee.map((emp) => (
-                        <div key={emp.id}>
-                            <div className="event__box py-2 px-2 border border-2 border-white">
-                                <h4 className="text-start px-3">Id: {emp.id}</h4>
-                                <h4 className="text-start px-3">Name: {emp.name}</h4>
-                                <h4 className="text-start px-3">Contact: {emp.email}</h4>
-                            </div>
-                            <button className='btn-l' onClick={() => { HandleRemove(emp.id) }}>Remove Employee</button>
-                        </div>
+                <Link className='btn btn-l w-50 mt-4' to='/regemployee' >register new Employee</Link>
+                <div className="py-3 list-items">
+                    <table className="col-12 table-bordered text-start text-white">
+                        <thead>
+                            <tr className='bg-white text-black text-center'>
+                                <th>
+                                    <h4 className=" px-3">Id</h4>
+                                </th>
+                                <th>
+                                    <h4 className="px-3">Name</h4>
+                                </th>
+                                <th>
+                                    <h4 className="px-3">Email</h4>
+                                </th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                    ))}
+                            {Employee.map((emp) => (
+                                <tr key={emp.id} className="event__box py-2 px-2">
+                                    <td><h4 className="text-start px-3">{emp.id}</h4></td>
+                                    <td><h4 className="text-start px-3">{emp.name}</h4></td>
+                                    <td><h4 className="text-start px-3">{emp.email}</h4></td>
+                                    {/* <td><button className='btn-l float-md-right' onClick={() => { HandleRemove(emp.id) }}>Remove Employee</button></td> */}
+                                    <td className='text-center'>
+                                        <button className='btn-l' data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => setId(emp.id)}>Remove Employee</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+                    {/* Remove Employee Popup div */}
+                    <div className="modal fade text-black" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Warning</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body text-start">
+                                    Are you Sure, you want to fire this Employee?
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary " onClick={() => { HandleRemove(Id) }} data-bs-dismiss="modal">Yes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
